7a74eb8f5c60f33ff65d39851221ea0b2c9eba3d
Pati Q <patiquindec@gmail.com>
2018-09-01 19:52:46 -0600

    OpenCV+ROS LISTO!

--- cusuco_test.cpp
+++ cusuco_test.cpp
@@ -28,6 +28,8 @@
 using namespace cv;
 using namespace std;
 
+#define PI 3.141592653589
+
 Mat HistogramMap(Mat image);
 Mat claheEqualization(Mat input);
 Mat LogFilter(Mat input), Exponential(Mat input);
@@ -39,9 +41,11 @@ int addfiles(string dir, vector<string> &files);
 Mat sobelFilter(Mat input);
 Point2f colorDetection(Mat input);
 Point2f coord;
-std_msgs::Float32;
-
-#define PI 3.141592653589
+std_msgs::Float32 ang_msg;
+float center = 640;
+float result;
+int areaFinal, areaDetect; /////////////////////////////////////DECLARAR
+int lowB, highB, lowG, highG, lowR, highR;
 
 void FunctionToHandlePublishedImage(const sensor_msgs::ImageConstPtr& msg);
 
@@ -52,139 +56,170 @@ cv_bridge::CvImagePtr cv_ptr_display;
 //namespace enc = sensor_msgs::image_encodings;
 int contadorDeImagenesRecibidas=0;
 
-
 main(int argc, char** argv){
+  leerParametrosDeControlDeArchivoDeTexto();
+  //Levantando del nodo ROS llamado "image_segmentation_node".
+  ros::init(argc, argv, "image_segmentation_node");
+  ros::NodeHandle nh_;
 
-    //Levantando del nodo ROS llamado "image_segmentation_node".
-    ros::init(argc, argv, "image_segmentation_node");
-    ros::NodeHandle nh_;
+  //Dandole al nodo la capacidad de recepcion de
+  //mensajes con imagenes
+  image_transport::ImageTransport it_(nh_);
 
-    //Dandole al nodo la capacidad de recepcion de
-    //mensajes con imagenes
-    image_transport::ImageTransport it_(nh_);
+  //Subscripción al topico "/usb_cam/image_raw", a traves del cual se recibiran las imagenes capturadas por la camara usb de su
+  //laptop. Se define un buffer de entrada de máximo 1 imágenes
+  sub_ourImageTopic_ = it_.subscribe("/usb_cam/image_raw", 1, FunctionToHandlePublishedImage);
+  //La función geoFunctionToHandlePublishedImage se ejecutara cada vez
+  //que un mensaje se reciba a través del tópico "/usb_cam/image_raw".
 
-    //Subscripción al topico "/usb_cam/image_raw", a traves del cual
-    //se recibiran las imagenes capturadas por la camara usb de su
-    //laptop. Se define un buffer de entrada de máximo 1 imágenes
-    sub_ourImageTopic_ = it_.subscribe("/usb_cam/image_raw", 1, FunctionToHandlePublishedImage);
-    //La función geoFunctionToHandlePublishedImage se ejecutara cada vez
-    //que un mensaje se reciba a través del tópico "/usb_cam/image_raw".
+  //Creando ventanas OpenCV para desplegar imagenes
+  //windowName0 = "Imagen de Intensidad";
+  //windowName1 = "Imagen Segmentada";
+  cvWaitKey(30); //Esta funcion ademas de hacer esperar al programa 30 ms, tambien fuerza a OpenCv a crear inmediatamente las ventanas
 
-    //Creando ventanas OpenCV para desplegar imagenes
-    //windowName0 = "Imagen de Intensidad";
-    //windowName1 = "Imagen Segmentada";
-	//printf("No me pegué ");
-    cvWaitKey(30); //Esta funcion ademas de hacer esperar al
-    //programa 30 ms, tambien fuerza a OpenCv a crear
-    //inmediatamente las ventanas
+  //El valor de X en la función ros::Rate loop_rate(X) indica el número de ciclos "while (ros::ok())" que ROS
+  //deberá realizar por segundo aproximadamente. Esta función trabaja en forma conjunta con la función loop_rate.sleep()
+  ros::Rate loop_rate(30);
 
-    //El valor de X en la función ros::Rate loop_rate(X)
-    //indica el número de ciclos "while (ros::ok())" que ROS
-    //deberá realizar por segundo aproximadamente. Esta función
-    //trabaja en forma conjunta con la función loop_rate.sleep()
-    ros::Rate loop_rate(30);
-
-    //ros::ok() es cero cuando ctrl+c es presionado en el teclado.
-    //Utilice esa combinación de teclas para salirse del programa.
-    while (ros::ok()){
-         //Dentro de la funcion "ros::spinOnce()" ROS ejecuta
-         //sus funciones. Los mensajes se atenderán solamente
-         //dentro de "ros::spinOnce()".
-         ros::spinOnce();
-         loop_rate.sleep();
+  //ros::ok() es cero cuando ctrl+c es presionado en el teclado.
+  //Utilice esa combinación de teclas para salirse del programa.
+  while (ros::ok()){
+   //Dentro de la funcion "ros::spinOnce()" ROS ejecuta sus funciones. Los mensajes se atenderán solamente dentro de "ros::spinOnce()".
+   ros::spinOnce();
+   loop_rate.sleep();
 	}
+  return 0;
+}
 
-    return 0;
+void leerParametrosDeControlDeArchivoDeTexto(){
+  int numeroDeDatosLeidos;
+  FILE *archivo;
+  char d1[256], d2[256];
+  int res;
+
+  printf("Leyendo los datos de entrada:\n");
+
+  //Abriendo archivo en mode de lectura
+  char nombreDeArchivo[256]="cusuco_cv.txt";
+  archivo = fopen(nombreDeArchivo, "r");
+  if (!archivo) {
+    printf("No se pudo abrir el archivo: current_control_parameters.txt\n");
+    exit(1);
+  }
+
+  //Leyendo datos linea por linea
+  //Leyendo path y nombre de imagen de entrada
+  res=fscanf(archivo, "%s %s\n", d1, d2);
+  if (res==0) {printf("Error leyendo dato No. %d de archivo de parametros de control\n", numeroDeDatosLeidos); exit(0);}
+  areaFinal=(int)d2;
+  cout << "Area final: " << areaFinal << endl;
+  numeroDeDatosLeidos++;
+
+  res=fscanf(archivo, "%s %s\n", d1, d2);
+  if (res==0) {printf("Error leyendo dato No. %d de archivo de parametros de control\n", numeroDeDatosLeidos); exit(0);}
+  areaDetect=(int)d2;
+  cout << "Area deteccion: " << areaDetect << endl;
+  numeroDeDatosLeidos++;
+
+  res=fscanf(archivo, "%s %s\n", d1, d2);
+  if (res==0) {printf("Error leyendo dato No. %d de archivo de parametros de control\n", numeroDeDatosLeidos); exit(0);}
+  lowB=(int)d2;
+  cout << "lowB: " << lowB << endl;
+  numeroDeDatosLeidos++;
+
+  res=fscanf(archivo, "%s %s\n", d1, d2);
+  if (res==0) {printf("Error leyendo dato No. %d de archivo de parametros de control\n", numeroDeDatosLeidos); exit(0);}
+  lowG=(int)d2;
+  cout << "lowG: " << lowG << endl;
+  numeroDeDatosLeidos++;
+
+  res=fscanf(archivo, "%s %s\n", d1, d2);
+  if (res==0) {printf("Error leyendo dato No. %d de archivo de parametros de control\n", numeroDeDatosLeidos); exit(0);}
+  cout << "lowR: " << lowR << endl;
+  numeroDeDatosLeidos++;
+
+  res=fscanf(archivo, "%s %s\n", d1, d2);
+  if (res==0) {printf("Error leyendo dato No. %d de archivo de parametros de control\n", numeroDeDatosLeidos); exit(0);}
+  highB=(int)d2;
+  cout <<"highB: " << highB << endl;
+  numeroDeDatosLeidos++;
+
+  res=fscanf(archivo, "%s %s\n", d1, d2);
+  if (res==0) {printf("Error leyendo dato No. %d de archivo de parametros de control\n", numeroDeDatosLeidos); exit(0);}
+  highG=(int)d2;
+  cout << "highG: " << highG << endl;
+  numeroDeDatosLeidos++;
+
+  res=fscanf(archivo, "%s %s\n", d1, d2);
+  if (res==0) {printf("Error leyendo dato No. %d de archivo de parametros de control\n", numeroDeDatosLeidos); exit(0);}
+  highR=(int)d2;
+  cout << "highR: " << highR << endl;
+  numeroDeDatosLeidos++;
+
+  fclose(archivo);
 }
 
 void FunctionToHandlePublishedImage(const sensor_msgs::ImageConstPtr& msg){
 	Mat claheEquaIm;         //Original and equalized images
 	Mat adapThresholdIm;             //thresholding techniques applied
-
-    //Extrayendo la imagen rgb del mensaje recibido
-//printf(" No me pegué x2 ");
-    try{
-      //cv_prt es un puntero a una estructura ROS que
-      //contiene un puntero a otra estructura OpenCV que
-      //a su vez contiene un puntero a la imagen rgb
-      //recibida.
+  //Extrayendo la imagen rgb del mensaje recibido
+  //printf(" No me pegué x2 ");
+  try{
+    //cv_prt es un puntero a una estructura ROS que contiene un puntero a otra estructura OpenCV que
+    //a su vez contiene un puntero a la imagen rgb recibida.
 	  Mat im1;
 	  Mat im2;
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
 	  //cv_ptr =toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-
-      //cv_prt_display es una copia de cv_prt. Esta copia
-      //se usará únicamente para visualización
-      cv_ptr_display = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-
+    //cv_prt_display es una copia de cv_prt. Esta copia se usará únicamente para visualización
+    cv_ptr_display = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
 		im1=cv_ptr->image;
 		im2=cv_ptr_display->image;
 
  		//HistogramMap(originalIms[i]).copyTo(EqHistogram);
-    	//Applying CLAHE equalization to each image
-    	claheEqualization(im2).copyTo(im2);
+  	//Applying CLAHE equalization to each image
+  	claheEqualization(im2).copyTo(im2);
 
-	    //Histogram obtained after CLAHE equalization
-    	//HistogramMap(claheEquaIm).copyTo(EqHistogram);
-    	//adaptiveThreshold(claheEquaIm).copyTo(adapThresholdIm)
-
-    	coord = colorDetection(im2);
-
-		  result = coord.x - center;
-    if(result > 0){
-      //Object to the right
-      result = abs(result);           //distance the car should move to one side or the other
-      result = angle(result);         //angle the car should move to the right
-      //Send message to move left motor RESULT angles to move to the right
-
-    }else if(result < 0){
-      //Object to the left
-      result = abs(result);
-      result = angle(result)*-1;        //angle the car should move to the left
-      //Send message to move right motor RESULT angles to move to the left
-    }else{                          //Object = Result
-      //Send message to keep moving with both motors working ======> Envia 0,0 (cero de direccion, cero de angulo)
-    }
-    	imshow("Coordinates", claheEquaIm); //show the thresholded image
-    	waitKey();
-    	destroyWindow("Coordinates");
-    	//equalizeHist(im2,im2);
-    	//namedWindow("Equalized image", WINDOW_AUTOSIZE);
-    	//imshow("Equalized image",im2);
-    	//cvWaitKey(30);
-
-
-    }
-    catch (cv_bridge::Exception& e){
-      ROS_ERROR("lalalalalalalcv_bridge exception: %s", e.what());
-      return;
-    }
-
-    contadorDeImagenesRecibidas++;
-	angle_pub.publish(msg);
-}
-
-
-//Function for directory reading and image retreiving
-int addfiles(string dir, vector<string> &files){   //receives the directory and a vector the dir's files
-  DIR *directory;           //directory stream
-  struct dirent *dirptr;    //structure which includes file serial number and filename string of entry
-  if((directory = opendir(dir.c_str())) == NULL){
-    cout << "Error opening directory " << endl;
-    return errno;          //error that ends the program
-  }
-  while((dirptr = readdir(directory)) != NULL){
-    if(!strcmp(dirptr->d_name, ".") || !strcmp(dirptr->d_name,"..")){
-        //do nothing
+    //Histogram obtained after CLAHE equalization
+  	//HistogramMap(claheEquaIm).copyTo(EqHistogram);
+  	//adaptiveThreshold(claheEquaIm).copyTo(adapThresholdIm)
+  	coord = colorDetection(im2);
+	  result = coord.x - center;
+    if(coord.x == 0 && coord.y == 0){
+      result = 999.0;
+    }else if(coord.x == 777777 && coord.y == 777777){
+      result = 888.0;
     }else{
-      files.push_back(string(dir + dirptr->d_name));  //saves full path directory as a string
-      sort(files.begin(),files.end());       //sorts files by filename
+      if(result > 0){
+        //Object to the right
+        result = abs(result);           //distance the car should move to one side or the other
+        result = angle(result);         //angle the car should move to the right
+        //Send message to move left motor RESULT angles to move to the right
+      }else if(result < 0){
+        //Object to the left
+        result = abs(result);
+        result = angle(result)*-1;        //angle the car should move to the left
+        //Send message to move right motor RESULT angles to move to the left
+      }else{                          //Object = Result
+        result = 0;
+        //Send message to keep moving with both motors working ======> Envia 0 (bien centrado)
+      }
     }
+    ang_msg = result;
+    imshow("Coordinates", claheEquaIm); //show the thresholded image
+    waitKey();
+    destroyWindow("Coordinates");
+    //equalizeHist(im2,im2);
+    //namedWindow("Equalized image", WINDOW_AUTOSIZE);
+    //imshow("Equalized image",im2);
+    //cvWaitKey(30);
+  }catch (cv_bridge::Exception& e){
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
   }
-  closedir(directory);
-  return 0;
+  //contadorDeImagenesRecibidas++;
+  angle_pub.publish(ang_msg);
 }
-
 
 Mat claheEqualization(Mat input){
   if(input.empty()){
@@ -207,7 +242,6 @@ Mat claheEqualization(Mat input){
   destroyWindow("CLAHEqualized Image");
   return equalizedIm;
 }
-
 
 Mat HistogramMap(Mat image){
   int hist_w = 512;
@@ -247,68 +281,42 @@ Mat HistogramMap(Mat image){
   return histImage;
 }
 
-
-Mat sobelFilter(Mat input){
-  Mat output;
-  int scale = 1;
-  int delta = 0;
-  int ddepth = CV_16S;
-  Mat grad_x, grad_y, abs_grad_x, abs_grad_y;
-  Sobel(input, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
-  Sobel(input, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
-  convertScaleAbs(grad_x, abs_grad_x);
-  convertScaleAbs(grad_y, abs_grad_y);
-  addWeighted(abs_grad_x, 0.5,abs_grad_y, 0.5, 0,output);
-  namedWindow("sobel Filter", WINDOW_AUTOSIZE);
-  imshow("sobel Filter", output);
-  waitKey();
-  destroyWindow("sobel Filter");
-  return output;
-}
-
-
 Point2f colorDetection(Mat input){
   Mat output;
-  int LowB, HighB, LowG, HighG, LowR, HighR;
-  LowB, LowG = 0;
-  HighB = 190;
-  HighG = 155;
-  LowR = 92;
-  HighR = 255;
   Mat imgThreshold;
   int iLastX = -1;
   int iLastY = -1;
   Mat imgLines = Mat::zeros(input.size(), CV_8UC3);
   Point2f finalPos;
 
-  inRange(input,Scalar(LowB,LowG,LowR),Scalar(HighB,HighG,HighR),imgThreshold);
-
+  inRange(input,Scalar(lowB,lowG,lowR),Scalar(highB,highG,highR),imgThreshold);
   //morphological opening (removes small objects from the foreground)
-  erode(imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-  dilate( imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-
+  erode(imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+  dilate(imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
   //morphological closing (removes small holes from the foreground)
-  dilate( imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-  erode(imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+  dilate(imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+  erode(imgThreshold, imgThreshold, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
   //Calculate the moments of the thresholded image
   Moments oMoments = moments(imgThreshold);
   double dM01 = oMoments.m01;
   double dM10 = oMoments.m10;
   double dArea = oMoments.m00;
+  cout << "Area detectada: " << dArea << endl;   //PRUEBAAAAAAAAAAAAAAAAAA
   // if the area <= 10000, I consider that there are no object in the image and it's because of the noise, the area is not zero
   //************HACER PRUEBAS CON LA BANDERA CON LA DISTANCIA DE 13 METROS*************
-  if (dArea > 10000)
-  {
+  if(dArea > areaFinal){
+    //DETENGASE
+    finalPos.x = 777777;
+    finalPos.y = 777777;
+  }else if(dArea > areaDetect){
     //calculate the position of the ball
     finalPos.x = dM10 / dArea;
     finalPos.y = dM01 / dArea;
+  }else{
+    cout << "No encontre nada, coordenadas (0,0)" << endl;
   }
 
-  if(finalPos.x == 0 && finalPos.y == 0){
-        //Manda msj de 'NO ENCUENTRA BANDERA': gira cada 45 grados tomando una foto
-		//hasta encontrar la bandera
-  }
   imshow("Thresholded Image", imgThreshold); //show the thresholded image
   waitKey();
   cout << finalPos.x << ", " << finalPos.y << endl;
@@ -323,6 +331,7 @@ Mat adaptiveThreshold(Mat input){
   waitKey();
   return output;
 }
+
 Mat medianBlurFilter(Mat input){
   Mat output;
   medianBlur(input,output,9);
@@ -336,7 +345,7 @@ Mat medianBlurFilter(Mat input){
 float angle(float distance){
   float output;
   float hfov = (54.61*(PI/180));       //angulo de hfov en radianes
-  float b = 1280;                      //pixeles totales en micrometros
+  float b = 1280;                      //pixeles totales en x
   float cita = hfov/2;
   output = (atan((2*distance*tan(cita*(PI/180.0)))/b)*180/PI);
   return output;                       //returns angle from center of the camera to one side or the other
